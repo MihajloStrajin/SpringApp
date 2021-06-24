0158b6d11f72fbd3e10f3cdfc1f4f25102cf7385
Mihajlo Strajin <strajin.mihajlo@uns.ac.rs>
2021-06-24 22:33:07 +0200

    Finished project

---
+++ Frontend_RVA/src/app/app.constants.ts
@@ -0,0 +1,5 @@
+export const KREDIT_URL = 'https://strajin-rva-backend.herokuapp.com/kredit';
+export const TIP_RACUNA_URL = 'https://strajin-rva-backend.herokuapp.com/tipRacuna';
+export const KLIJENT_URL = 'https://strajin-rva-backend.herokuapp.com/klijent';
+export const RACUN_URL = 'https://strajin-rva-backend.herokuapp.com/racun';
+export const RACUNI_ZA_KLIJENTA_URL = 'https://strajin-rva-backend.herokuapp.com/racuniZaKlijenta';
